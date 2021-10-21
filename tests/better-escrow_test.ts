3a04acd142e22c40fbf002164cbc26ac9199a5cb
Peter Nash <34408740+dartman10@users.noreply.github.com>
2021-10-21 15:02:44 -0400

    Update better-escrow_test.ts

--- tests/better-escrow_test.ts
+++ tests/better-escrow_test.ts
@@ -11,28 +11,36 @@ Clarinet.test({
         let buyer     = accounts.get('wallet_8')!;
         let mediator  = accounts.get('wallet_7')!;
 
+        /*
+        ------------------------------------------------------------ 
+           Test the basic functions.
+        ------------------------------------------------------------- 
+        */
         let block = chain.mineBlock([
-           /* Test the basic functions. */
            Tx.contractCall('better-escrow', 'about', [], seller.address),
            Tx.contractCall('better-escrow', 'status-of-contract', [], seller.address),
         ]);
-        /* assertEquals(block.receipts.length, 1); */
+        assertEquals(block.receipts.length, 2);
         assertEquals(block.receipts[0].result.expectOk(), '"better escrow is the escrow.com killer"');
         assertEquals(block.receipts[1].result.expectOk(), '[u0, u0, u0]');
 
+        /*
+        ------------------------------------------------------------ 
+           Simulate a smooth escrow transaction.                      
+        ------------------------------------------------------------- 
+        */
         block = chain.mineBlock([
-            /* Simulate a smooth escrow transaction. */
             Tx.contractCall('better-escrow', 'bill-create', [], seller.address),
             Tx.contractCall('better-escrow', 'bill-accept', [], buyer.address),
             Tx.contractCall('better-escrow', 'fund-seller', [], seller.address),
             Tx.contractCall('better-escrow', 'fund-buyer', [], buyer.address),
             Tx.contractCall('better-escrow', 'fund-release', [], buyer.address),
          ]);
-         /* assertEquals(block.receipts.length, 1); */
+         assertEquals(block.receipts.length, 5);
          assertEquals(block.receipts[0].result.expectOk().expectOk(), '[u1, u0, u0]');
          assertEquals(block.receipts[1].result.expectOk().expectOk(), '[u1, u1, u0]');
          assertEquals(block.receipts[2].result.expectOk().expectOk(), '[u2, u1, u0]');
          assertEquals(block.receipts[3].result.expectOk().expectOk(), '[u2, u2, u0]');
-         assertEquals(block.receipts[3].result.expectOk().expectOk(), '[u2, u2, u0]');
+         assertEquals(block.receipts[4].result.expectOk().expectOk(), '[u2, u3, u0]');
     },
 });
