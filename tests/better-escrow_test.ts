d4e025d24f7599aadfd7827198c6bf9971715b00
Peter Nash <34408740+dartman10@users.noreply.github.com>
2021-10-21 10:31:32 -0400

    Update better-escrow_test.ts

--- tests/better-escrow_test.ts
+++ tests/better-escrow_test.ts
@@ -3,24 +3,27 @@ import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarine
 import { assertEquals } from 'https://deno.land/std@0.90.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
+    name: "Testing bill-create function.",
     async fn(chain: Chain, accounts: Map<string, Account>) {
+
+        /* Assign wallets to be used for simulating the escrow participants - seller, buyer, mediator. */
+        let seller    = accounts.get('wallet_9')!;
+        let buyer     = accounts.get('wallet_8')!;
+        let mediator  = accounts.get('wallet_7')!;
+
         let block = chain.mineBlock([
             /* 
              * Add transactions with: 
              * Tx.contractCall(...)
             */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
-
-        block = chain.mineBlock([
-            /* 
-             * Add transactions with: 
-             * Tx.contractCall(...)
+            /* Tx.contractCall('better-escrow', 'bill-create', [], seller.address),
             */
+           Tx.contractCall('better-escrow', 'about', [], seller.address),
+
         ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
+        assertEquals(block.receipts.length, 1);
+        assertEquals(block.receipts[0].result.expectOk(), '"better escrow is the escrow.com killer"');
+        /* assertEquals(block.receipts[0], "(ok (ok [u1, u0, u0]))"); *?
+        /* assertEquals(block.height, 2); */
     },
 });
